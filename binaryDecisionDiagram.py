ff9742ca4b9e317cffc3e3a6662bff6fd1d53521
Maximilián Strečanský <98816862+xstrecansky@users.noreply.github.com>
2022-04-10 22:39:03 +0200

    Update binaryDecisionDiagram.py

--- binaryDecisionDiagram.py
+++ binaryDecisionDiagram.py
@@ -4,10 +4,35 @@ class Node:
         self.left = None
         self.right = None
         self.value = value
+    #Funkcia na vytvorenie binarneho diagramu
+    def BDD_create(self, fList, poradie):
+        if len(fList)>2:
+            self.value = fList
+            self.left = self.BDD_create((leftString(fList,poradie[0])), (fList[1:]))
+            self.right = self.BDD_create((rightString(fList,poradie[0])), (fList[1:]))
+            return self
+        return self
+    #Funkcia na zistenie hodnoty podla vstupu
+    def BDD_use(self, vstup):
+        for item in vstup:
+            if item == '0':
+                self = self.left
+            if item == '1':
+                self = self.right
+        return self.value
+    #Inorder vypisanie stromu
+    def inorder(self, root): 
+        if(root is None ): 
+            return
+        self.inorder(self.left) 
+        print(self.value,end = ' ') 
+        self.inorder(self.right)
 #Trida binarneho rozhodovacieho diagramu
 class BDD:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, poradie):
+        self.root = Node()
+        self.values = 0
+        self.poradie = poradie
 #Vypise vsetky zaporne prvky
 def leftString(fList, letter):
     nonA = ""
@@ -34,9 +59,11 @@ def rightString(fList, letter):
         if item.find(letter)==-1:
             posA = posA + item + ' '
     return posA
-
+#Zadavame v tvare A.B+C.E+!A.E.F...
 bfunkcia = input('Zadaj funkciu v DNF:\n')
 fList = bfunkcia.split('+')
+#Zadavame v tvare ABCDE...
 poradie = input('Zadaj poraadie fList:\n')
-
-print(leftString(fList,poradie[0]) + '\n' + rightString(fList,poradie[0]))
+root = Node(fList)
+root.BDD_create(fList, poradie)
+print(root.inorder(root))
