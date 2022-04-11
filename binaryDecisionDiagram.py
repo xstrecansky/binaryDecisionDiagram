7c43019f13ecc54200a90f186e7bf1ab9d488f7e
Maximilián Strečanský <98816862+xstrecansky@users.noreply.github.com>
2022-04-11 18:46:10 +0200

    Add files via upload

--- binaryDecisionDiagram.py
+++ binaryDecisionDiagram.py
@@ -1,82 +1,84 @@
 #Trieda jednej nody
-from operator import le
-
-
-class Node:
+class Node(object):
     def __init__(self, value):
         self.left = None
         self.right = None
         self.value = value
-    #Funkcia na vytvorenie binarneho diagramu
-    def BDD_create(self, fList, poradie):
-        if len(fList)>2:
-            self.value = fList
-            self.left = self.BDD_create((leftString(fList,poradie[0])), (fList[1:]))
-            self.right = self.BDD_create((rightString(fList,poradie[0])), (fList[1:]))
-            return self
-        return self
-    #Funkcia na zistenie hodnoty podla vstupu
-    def BDD_use(self, vstup):
-        for item in vstup:
-            if item == '0':
-                self = self.left
-            if item == '1':
-                self = self.right
-        return self.value
-    #Inorder vypisanie stromu
-    def inorder(self, root): 
-        if(root is None): 
-            return
-        self.inorder(self.left) 
-        print(self.value,end = ' ') 
-        self.inorder(self.right)
 #Trida binarneho rozhodovacieho diagramu
-class BDD:
-    def __init__(self, poradie):
-        self.root = Node()
+class BDD(object):
+    def __init__(self, poradie, fList):
+        self.root = Node(fList)
         self.values = 0
         self.poradie = poradie
 #Vypise vsetky zaporne prvky
 def leftString(fList, letter):
-    nonA = []
-    for item in fList:
-        new = item.replace('!'+letter,'')
-        if item.find('!'+letter)==0:
-            nonA.append(new)
-        if item.find(letter)==-1:
-            nonA.append(item)
-    if '' in nonA:
-        nonA.remove('')
-    if len(nonA)==0:
-        nonA.append('0')
-        return nonA
-    return list(dict.fromkeys(nonA))
+    #Pripad kedy mame !C v lavej strane -> vzdy 1
+    if '!'+letter in fList:
+        return '1'
+    else:
+        nonA = []
+        for item in fList:
+            #Pripad kedy sa nachadza vo funkcii !C
+            if item.find('!'+letter)==0:
+                #Odstranime pismeno a pridame do listu
+                new = item.replace('!'+letter,'')
+                nonA.append(new)
+                #Ak sa nenachadza !C vo funkcii pridame
+            if item.find(letter)==-1:
+                nonA.append(item)
+        if '' in nonA:
+            nonA.remove('')
+        if len(nonA)==0:
+            nonA.append('0')
+            return nonA
+        #Odstranime duplikaty
+        return list(dict.fromkeys(nonA))
 #Vypise vsetky kladne prvky
 def rightString(fList, letter):
-    posA = []
-    for item in fList:
-        new = item.replace(letter,'')
-        if item.find(letter)==0 and item[0]!='!':
-            posA.append(new)
-        if item.find(letter)==-1:
-            posA.append(item)
-    if '' in posA:
-        posA.remove('')
-    if len(posA)==0:
-        posA.append('1')
-        return posA
-    return list(dict.fromkeys(posA))
-#Zadavame v tvare AB+CE+!AEF...
-bfunkcia = input('Zadaj funkciu v DNF:\n')
+    #Pripad kedy mame C v pravej strane -> vzdy 1
+    if letter in fList:
+        return '1'
+    else:
+        posA = []
+        for item in fList:
+            #Pripad kedy sa nachadza vo funkcii C
+            if item.find(letter)==0 and item[0]!='!':
+                #Odstranime pismeno a pridame do listu
+                new = item.replace(letter,'')
+                posA.append(new)
+            #Ak sa nenachadza C vo funkcii pridame
+            if item.find(letter)==-1:
+                posA.append(item)
+        if '' in posA:
+            posA.remove('')
+        if len(posA)==0:
+            posA.append('1')
+            return posA
+        #Odstranime duplikaty
+        return list(dict.fromkeys(posA))
+#Preorder vypisanie stromu
+def preorder(root): 
+    if root:
+        print(root.value)
+        preorder(root.left)
+        preorder(root.right)
+    return root
+#Funkcia na vytvorenie binarneho diagramu
+def BDD_create(root, poradie):
+    if len(poradie)==0:
+        return None
+    root.left = Node((leftString(root.value,poradie[0])))
+    root.right = Node((rightString(root.value,poradie[0])))
+    root.left = BDD_create(root.left, poradie[1:])
+    root.right = BDD_create(root.right, poradie[1:])
+    return root
+#Zadavame v tvare A!C+ABC+!AB+!BC
+#bfunkcia = input('Zadaj funkciu v DNF:\n')
+bfunkcia = "A!C+ABC+!AB+!BC"
 fList = bfunkcia.split('+')
-#Zadavame v tvare ABCDE...
-poradie = input('Zadaj poraadie fList:\n')
-left = fList
-right = fList
-print(fList)
-for element in range(0, len(poradie)):
-    left = leftString(left, poradie[element])
-    right = rightString(right, poradie[element])
-    print(left)
-    print(right)
-
+#Zadavame v tvare ABCDE
+#poradie = input('Zadaj poraadie fList:\n')
+poradie = "ABC"
+bddroot = BDD(poradie, fList)
+bddroot.root = BDD_create(bddroot.root, poradie)
+preorder(bddroot.root)
