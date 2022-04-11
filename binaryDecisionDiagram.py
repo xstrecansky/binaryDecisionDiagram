a576392bc99b7b4a18b6a144d35a557b5d4f980c
Maximilián Strečanský <98816862+xstrecansky@users.noreply.github.com>
2022-04-11 15:59:28 +0200

    Add files via upload

--- binaryDecisionDiagram.py
+++ binaryDecisionDiagram.py
@@ -1,4 +1,7 @@
 #Trieda jednej nody
+from operator import le
+
+
 class Node:
     def __init__(self, value):
         self.left = None
@@ -22,7 +25,7 @@ class Node:
         return self.value
     #Inorder vypisanie stromu
     def inorder(self, root): 
-        if(root is None ): 
+        if(root is None): 
             return
         self.inorder(self.left) 
         print(self.value,end = ' ') 
@@ -35,35 +38,45 @@ class BDD:
         self.poradie = poradie
 #Vypise vsetky zaporne prvky
 def leftString(fList, letter):
-    nonA = ""
+    nonA = []
     for item in fList:
-        if len(item)==2:
-            new = item.replace('!'+letter,'')
-        else:
-            new = item.replace('!'+letter+'.','')
+        new = item.replace('!'+letter,'')
         if item.find('!'+letter)==0:
-            nonA = nonA + new + ' ' 
+            nonA.append(new)
         if item.find(letter)==-1:
-            nonA = nonA + item + ' '   
-    return nonA
+            nonA.append(item)
+    if '' in nonA:
+        nonA.remove('')
+    if len(nonA)==0:
+        nonA.append('0')
+        return nonA
+    return list(dict.fromkeys(nonA))
 #Vypise vsetky kladne prvky
 def rightString(fList, letter):
-    posA = ""
+    posA = []
     for item in fList:
-        if len(item)==1:
-            new = item.replace(letter,'')
-        else:
-            new = item.replace(letter+'.','')
-        if item.find(letter)==0:
-            posA = posA + new + ' '
+        new = item.replace(letter,'')
+        if item.find(letter)==0 and item[0]!='!':
+            posA.append(new)
         if item.find(letter)==-1:
-            posA = posA + item + ' '
-    return posA
-#Zadavame v tvare A.B+C.E+!A.E.F...
+            posA.append(item)
+    if '' in posA:
+        posA.remove('')
+    if len(posA)==0:
+        posA.append('1')
+        return posA
+    return list(dict.fromkeys(posA))
+#Zadavame v tvare AB+CE+!AEF...
 bfunkcia = input('Zadaj funkciu v DNF:\n')
 fList = bfunkcia.split('+')
 #Zadavame v tvare ABCDE...
 poradie = input('Zadaj poraadie fList:\n')
-root = Node(fList)
-root.BDD_create(fList, poradie)
-print(root.inorder(root))
+left = fList
+right = fList
+print(fList)
+for element in range(0, len(poradie)):
+    left = leftString(left, poradie[element])
+    right = rightString(right, poradie[element])
+    print(left)
+    print(right)
+
