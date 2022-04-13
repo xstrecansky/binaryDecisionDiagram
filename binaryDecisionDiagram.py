438ea0b56adf5edac1145654c0fb9dc16e98bf8d
Maximilián Strečanský <98816862+xstrecansky@users.noreply.github.com>
2022-04-13 15:47:50 +0200

    Add files via upload

--- binaryDecisionDiagram.py
+++ binaryDecisionDiagram.py
@@ -1,4 +1,3 @@
-COUNT = [10]
 #Trieda jednej nody
 class Node(object):
     def __init__(self, value, id):
@@ -14,16 +13,6 @@ class Node(object):
         print(self.value)
         if self.right:
             self.right.inorder()
-    #Rekurzivne prejdeme vsetky, prvky a ak najdeme vstupnu hodnotu, vratime nodu
-    def checkForDuplicates(self, value):
-        if value == self.value:
-            return self
-        if self.left:
-            return self.left.checkForDuplicates(value)
-        if self.right:
-            return self.right.checkForDuplicates(value)
-        return None
-
 #Trida binarneho rozhodovacieho diagramu
 class BDD(object):
     def __init__(self, poradie, fList):
@@ -32,8 +21,6 @@ class BDD(object):
         self.poradie = poradie
     def incValues(self):
         self.values += 1
-        return self.values
-    def getValues(self):
         return self.values
     #Funckia na vytvorenie binarneho diagramu
     #Bez oplimalizacie a odstranenia duplikatov
@@ -48,38 +35,60 @@ class BDD(object):
     #Funkcia na vytvorenie binarneho diagramu
     def BDD_create(self, root, poradie):
         if poradie:
-            #TODO:
-            if '1' not in root.value and '0' not in root.value:
-                tempLeftString = leftString(root.value,poradie[0])
-                tempRightString = rightString(root.value,poradie[0])
-                if root.checkForDuplicates(tempLeftString) == None:
-                    root.left = Node(tempLeftString, self.incValues())
+            tempLeft = leftString(root.value,poradie[0])
+            tempRight = rightString(root.value,poradie[0])
+            if root.value == tempLeft and root.value == tempRight:
+                poradie = poradie[1:]
+                tempLeft = leftString(tempLeft, poradie[0])
+                tempRight = rightString(tempRight, poradie[0])
+            if tempLeft == tempRight:
+                if ('1' or '0') in tempLeft:
+                    answerRoot = checkForDuplicates(self.root, tempLeft)
+                    if answerRoot:
+                        root.left = answerRoot
+                        root.right = root.left
+                else:
+                    root.left = Node(tempLeft, self.incValues())
+                    root.right = root.left
                     root.left = self.BDD_create(root.left, poradie[1:])
-                #Ak najdeme duplikat, nastavime ho ako smernik na lavy prvok
+                return root
+            if ('1' or '0') in tempLeft:
+                answerRoot = checkForDuplicates(self.root, tempLeft)
+                if answerRoot:
+                    root.left = answerRoot
                 else:
-                    root.left = root.checkForDuplicates(tempLeftString)
-                if root.checkForDuplicates(tempRightString) == None:
-                    root.right = Node(tempRightString, self.incValues())
-                    root.right = self.BDD_create(root.right, poradie[1:])
-                #Ak najdeme duplikat, nastavime ho ako smernik na pravy prvok
+                    root.left = Node(tempLeft, self.incValues())
+            else:
+                root.left = Node(tempLeft, self.incValues())
+                root.left = self.BDD_create(root.left, poradie[1:])
+            if ('1' or '0') in tempRight:
+                answerRoot = checkForDuplicates(self.root, tempRight)
+                if answerRoot:
+                    root.right = answerRoot
                 else:
-                    root.right = root.checkForDuplicates(tempRightString)
+                    root.right = Node(tempRight, self.incValues())
+            else:
+                root.right = Node(tempRight, self.incValues())
+                root.right = self.BDD_create(root.right, poradie[1:])
             return root
-        return None
+        return root
     #Metoda pre vypis vysledku
     def BDD_use(self, combination):
         tempRoot = self.root
         for letter in combination:
+            if not tempRoot.right and not tempRoot.left:
+                print(tempRoot.value)
+                return
             if letter =='0':
                 #Pozrieme sa ci vobec existuje lavy prvok kezde
                 #v pripade rovnakeho praveho a laveho prvku ulozime iba pravy
-                if tempRoot.left:
-                    tempRoot = tempRoot.left
-                else:
-                    if tempRoot.right:
-                        tempRoot = tempRoot.right
-            else:
+                tempRoot = tempRoot.left
+            if letter =='1':
                 tempRoot = tempRoot.right
+            #Chyba
+            else:
+                print('-1')
+                return
         print(tempRoot.value)
     #Spusti vypisanie 2D prvkov
     def print2D(self):
@@ -132,10 +141,20 @@ def rightString(fList, letter):
             return posA
         #Odstranime duplikaty
         return list(dict.fromkeys(posA))
+#Rekurzivne prejdeme vsetky, prvky a ak najdeme vstupnu hodnotu, vratime nodu
+def checkForDuplicates(root, value):
+    if root:
+        if value == root.value:
+            return root
+        if root.left:
+            return checkForDuplicates(root.left, value)
+        if root.right:
+            return checkForDuplicates(root.right, value)
 #Vypisanie stromu 2D z internetu
 #https://www.geeksforgeeks.org/print-binary-tree-2-dimensions/
-def print2DUtil(root, space) :
-    if (root == None) :
+def print2DUtil(root, space):
+    COUNT = [10]
+    if (root == None):
         return
     space += COUNT[0]
     print2DUtil(root.right, space)
@@ -148,19 +167,19 @@ def print2DUtil(root, space) :
 #Zadavame v tvare A!C+ABC+!AB+!BC
 while(True):
     fList = input('Zadaj funkciu v DNF:\n').split('+')
-    #Zadavame v tvare ABCDE
-    poradie = input('Zadaj poradie:\n')+ " "
-    #Testujeme pre optimalizovy diagram
+    poradie = input("Zadaj poradie:\n")
+
     bddroot = BDD(poradie, fList)
     bddroot.root = bddroot.BDD_create(bddroot.root, poradie)
     bddroot.print2D()
-    print(bddroot.getValues())
-    """
-    print("--------------------------------------------------------------------------------\n")
+    
     #Testujeme pre neoptimalizovany diagram
-    bad = BDD(poradie, fList)
-    bad.root = bad.BDD_createWithDuplicates(bad.root, poradie)
-    bad.print2D()
-    """
+    #bad = BDD(poradie, fList)
+    #bad.root = bad.BDD_createWithDuplicates(bad.root, poradie)
+    #bad.print2D()
+    while(True):
+        hodnota = []
+        hodnota.append(input('Zadaj hodnotu:\n'))
+        print(checkForDuplicates(bddroot.root, hodnota))
     kombinacia = input('Zadaj kombinaciu:\n')
     bddroot.BDD_use(kombinacia)
